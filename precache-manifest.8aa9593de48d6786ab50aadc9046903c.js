029ad0f23b7b7f638c9065dfc78dcb011491d963
neelesh7singh <neeleshsingh767@gmail.com>
2020-06-15 02:34:01 +0530

    Updates

---
+++ precache-manifest.8aa9593de48d6786ab50aadc9046903c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dec059fbec1ee71a0f4c1196cdd20c8",
+    "url": "/react-microInteraction/index.html"
+  },
+  {
+    "revision": "7f795aa7438494e7dd36",
+    "url": "/react-microInteraction/static/css/main.32fc3453.chunk.css"
+  },
+  {
+    "revision": "d9a6cd618728c0e44c12",
+    "url": "/react-microInteraction/static/js/2.e8ae95fa.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/react-microInteraction/static/js/2.e8ae95fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f795aa7438494e7dd36",
+    "url": "/react-microInteraction/static/js/main.8773ddd5.chunk.js"
+  },
+  {
+    "revision": "08d7b336a0839d2ffeaf",
+    "url": "/react-microInteraction/static/js/runtime-main.40217ac3.js"
+  },
+  {
+    "revision": "148da1c4983c086cfadfcd2237421dcc",
+    "url": "/react-microInteraction/static/media/chevron.148da1c4.svg"
+  },
+  {
+    "revision": "b0200bf7cbbb26c32989e7a0e4d19eef",
+    "url": "/react-microInteraction/static/media/close.b0200bf7.svg"
+  },
+  {
+    "revision": "492acdf2b27ec78464b9eec70505883e",
+    "url": "/react-microInteraction/static/media/down-arrow.492acdf2.svg"
+  },
+  {
+    "revision": "a5de5125f59d7ac9e887e4e298fa0b77",
+    "url": "/react-microInteraction/static/media/product.a5de5125.png"
+  }
+]);
